aea72dcff13bd673386d51ae6fd91971e1ff7eb1
tiaraprmtsr <tiarapermata580@gmail.com>
2022-06-23 11:06:56 +0700

    Initial commit

---
+++ src/BinaryTree/BinaryTree.java
@@ -0,0 +1,15 @@
+package BinaryTree;
+
+import org.w3c.dom.Node;
+
+public class BinaryTree {
+    public static void main(String[] args) {
+        BinaryTree tree = new BinaryTree();
+
+        tree.root = new Node (item: 1);
+        tree.root.left  = new Node (item: 2;
+        tree.root.right  = new Node (item: 3;
+        tree.root.left.left  = new Node (item: 4;
+
+    }
+}
